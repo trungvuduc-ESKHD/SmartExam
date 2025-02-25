392db1783f97e331db229fc9c66410e77460874f
trungvu204 <trung.vuduc@eurofinsasia.com>
2025-02-25 17:35:59 +0700

    Update BEST_PDF_STUDY_APP.py

--- BEST_PDF_STUDY_APP.py
+++ BEST_PDF_STUDY_APP.py
@@ -74,8 +74,9 @@ st.markdown(hide_default_format, unsafe_allow_html=True)
 #Apis
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+SUPABASE_ANON_KEY = st.secrets["SUPABASE_ANON_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 # Resetting quiz state
 
